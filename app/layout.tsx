2ccc42605bb4a87899fec9a2632511df81811da6
Abdirahman <abdirahmanxabokar@gmail.com>
2024-09-27 21:09:20 +0200

    minor changes to layout of homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
     <div className="navbar bg-base-100">
       <div className="flex-1">
         <Link href="/" className="btn btn-ghost text-xl">
-         📸 ShotStore
+         📸 FrameVault
         </Link>
       </div>
       <div className="flex-none">
