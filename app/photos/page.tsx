814fc6645a978fc657d9d3e47cb1c826e4a0cf86
Abdirahman <abdirahmanxabokar@gmail.com>
2024-10-22 16:45:52 +0200

    more photos for store

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -5,6 +5,12 @@ export default function PhotosPage() {
     "https://w.wallhaven.cc/full/4x/wallhaven-4xp21l.jpg",
     "https://w.wallhaven.cc/full/n6/wallhaven-n6m7lq.jpg",
     "https://w.wallhaven.cc/full/6k/wallhaven-6kw32q.jpg",
+    "https://w.wallhaven.cc/full/od/wallhaven-odw8e7.jpg",
+    "https://w.wallhaven.cc/full/d5/wallhaven-d5k623.jpg",
+    "https://w.wallhaven.cc/full/2e/wallhaven-2e95xg.jpg",
+    "https://w.wallhaven.cc/full/95/wallhaven-95rrq8.jpg",
+    "https://w.wallhaven.cc/full/1k/wallhaven-1kr939.jpg",
+    "https://w.wallhaven.cc/full/83/wallhaven-83km1j.jpg",
   ];
 
   return (
