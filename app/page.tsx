f24c37f9e29560eec649dc3dd2f6b4f85d47d53f
abdirahman <abdirahmanxabokar@gmail.com>
2024-11-05 16:57:33 +0100

    minor layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -26,4 +26,4 @@ export default function Home() {
     </main>
   );
 }
-//vercel template ? 1. front page image slider, 2. wallpaper api 3. sign up 4. embedded unsubscribe
+//vercel template ? 1. front page image slider, 2. wallpaper api 3. sign up 4. embedded unsubscribe 5.more subscription tiers?
