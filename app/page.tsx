8e949affbfe11e882d5069aaa900040e36ab23fb
abdirahman <abdirahmanxabokar@gmail.com>
2024-11-21 15:15:40 +0100

    minor layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -26,4 +26,4 @@ export default function Home() {
     </main>
   );
 }
-//1. front page image slider / wallhaven api? 2. sign up
+//1. front page image slider / wallhaven api? 2. sign up with acc/pass
